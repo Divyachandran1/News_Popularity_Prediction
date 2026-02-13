fe42c95d243b48e736a423f4d859bab11ac68f82
Divyachandran1 <divyachandhran20@gmail.com>
2026-02-13 22:04:16 +0530

    Delete Open_Embeddings.py

--- Open_Embeddings.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-data = np.load("final_embeddings.npy", allow_pickle=True)
-
-print("Type of data:", type(data))
-print("Shape:", data.shape)
-print("First embedding vector:", data[0])
